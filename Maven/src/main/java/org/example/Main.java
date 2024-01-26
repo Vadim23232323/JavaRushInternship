8b27c85b4fbf860b805f48a7f634512868fe382b
Vadim_dubovskiy <Vadim_milo.net@mail.ru>
2024-01-26 18:52:07 +0300

    Maven: фазы, плагины, зависимости и сборка

---
+++ Maven/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getText());
+    }
+
+    public static String getText(){
+        String text = "Hello Maven";
+        return text;
+    }
+}
